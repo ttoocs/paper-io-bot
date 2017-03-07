f9c2772db181a30406861cc12f95baf9995691f1
ttoocs <ttoocs@gmail.com>
2017-03-07 10:12:31 -0700

    Inital script from tampermonkey.

---
+++ bot1/main.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://paper-io.com/
+// @grant        none
+// ==/UserScript==
+
+var verson = 0.1;
+
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
