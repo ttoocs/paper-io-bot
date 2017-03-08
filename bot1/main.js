b770822ef93c3c323b69acd1d12ad7cbaa563ceb
ttoocs <ttoocs@gmail.com>
2017-03-07 22:49:40 -0700

    Apprently, I need todo the impossible..?

--- bot1/main.js
+++ bot1/main.js
@@ -14,7 +14,9 @@
 // Construction:
 //
 // Structure:
-//  Have a function to genrate a map[]
+//  Have a function to genrate a map[][]
+//    //each element being an int(?), of the player who has it.
+//    // -1 for white, and -2 for wall.
 //  Pass this map to the AI
 //  Have output-functions (outside of the AI)
 //  This allows the AI to be the same while changing the IO, to permit easy changing of anti-hacker stuff.
@@ -26,6 +28,65 @@
     // Your code here...
 })();
 
+sendKey = function(keycode){
+
+  //var keyboardEvent = document.createEvent("KeyboardEvent");
+  //var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? "initKeyboardEvent" : "initKeyEvent";
+
+
+  var e = new KeyboardEvent("keydown",
+                    {bubbles : true, cancelable : true, key : keycode }
+                  // "keypress", // event type : keydown, keyup, keypress
+                  //  true, // bubbles
+                  //  true, // cancelable
+                  //  window, // viewArg: should be window
+                  //  false, // ctrlKeyArg
+                  //  false, // altKeyArg
+                  //  false, // shiftKeyArg
+                  //  false, // metaKeyArg
+                  //  0, // keyCodeArg : unsigned long the virtual key code, else 0
+                  //  0 // charCodeArgs : unsigned long the Unicode character associated with the depressed key, else 0
+                  );
+  //console.log(keyboardEvent.key);
+  console.log(e);
+  document.dispatchEvent(e);
+};
+
+//Simple go-in-a-square
+enableBot = function(){
+  setInterval(squareLoop,300);
+};
+
+var squareLoopState = 0;
+squareLoop = function(){
+  if(squareLoopState === 0){
+    sendKey("ArrowDown");
+    squareLoopState++;
+  }else if(squareLoopState === 1){
+    sendKey("ArrowRight");
+    squareLoopState++;
+  }else if(squareLoopState === 2){
+    sendKey("ArrowUp");
+    squareLoopState++;
+  }else if(squareLoopState === 3){
+    sendKey("ArrowLeft");
+    squareLoopState = 0;
+  }
+
+};
+
+//Takes in some 2D pixles values,
+//  and generates 2D coordinates.
+toTileCoords = function(){
+
+};
+//Takes in the 2D tile coordinates,
+// and generates 2D pixle coordinates.
+toPxCoords = function(){
+
+};
+
+//works in console (last tested: March 7, 2017)
 getPlayers = function(){
   var scores = document.getElementById('scores');
   var players = [];
